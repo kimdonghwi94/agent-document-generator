6d4598c3f35a80a5fbb39ef5465ae4b15de7e151
donghwi <kimdonghwi94@gmail.com>
2025-08-28 17:52:05 +0900

    fix: app health

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.agent_document_generator.__main__ import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    import uvicorn
+    from src.agent_document_generator.__main__ import create_app
+    uvicorn.run(
+       create_app(),
+       host="0.0.0.0",
+       port=8000,
+       reload=True
+   )
